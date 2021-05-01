74525a680dfd535d6f09d4c8ee1a9a875268104c
MatheusADSantos <matheusdevmobile2020@gmail.com>
2021-05-01 16:27:47 -0300

    First Commit

---
+++ spread.php
@@ -0,0 +1,20 @@
+<?php
+// $arrayOdd = [1, 3, 5, 7, 9];
+// $arrayEven = [0, 2, 4, 6, 8];
+
+// $arrayTotal = [...$arrayOdd, ...$arrayEven];
+// print_r($arrayTotal);
+
+// echo '<br> Todos os números: ' . $arrayTotal;
+// echo $arraOdd[1];
+
+
+$arrayNames = [
+  'Matheus',
+  'Marcela',
+  'Carol',
+];
+print_r($arrayNames);
+
+echo '<br> Names: ' . $arrayNames;
+echo '<br> Name: ' . $arrayNames[1];
