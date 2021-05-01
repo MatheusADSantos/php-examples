dc50e666291d8667a2a4e3b34320b19354333e74
MatheusADSantos <matheusdevmobile2020@gmail.com>
2021-05-01 14:39:34 -0300

    Other example ...

--- spread.php
+++ spread.php
@@ -1,9 +1,20 @@
 <?php
-$arrayOdd = [1, 3, 5, 7, 9];
-$arrayEven = [0, 2, 4, 6, 8];
+// $arrayOdd = [1, 3, 5, 7, 9];
+// $arrayEven = [0, 2, 4, 6, 8];
 
-$arrayTotal = [...$arrayOdd, ...$arrayEven];
-print_r($arrayTotal);
+// $arrayTotal = [...$arrayOdd, ...$arrayEven];
+// print_r($arrayTotal);
 
-echo '<br> Todos os números: ' . $arrayTotal;
-echo $arraOdd[1];
+// echo '<br> Todos os números: ' . $arrayTotal;
+// echo $arraOdd[1];
+
+
+$arrayNames = [
+  'Matheus',
+  'Marcela',
+  'Carol',
+];
+print_r($arrayNames);
+
+echo '<br> Names: ' . $arrayNames;
+echo '<br> Name: ' . $arrayNames[1];
